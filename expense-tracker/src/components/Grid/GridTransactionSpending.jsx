84dc752aab402064be28ad12b3f979a97e4c5ccc
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-05-12 01:00:07 +0530

    grid optimize

--- expense-tracker/src/components/Grid/GridTransactionSpending.jsx
+++ expense-tracker/src/components/Grid/GridTransactionSpending.jsx
@@ -14,12 +14,12 @@ const GridTransactionSpending = () => {
   //  console.log(expenceList[0]);
   return (
     <Grid container spacing={5} height={"50vh"} margin={"12px 12px"}> 
-      <Grid item xs={12} md={7} lg={7} >
+      <Grid item xs={12} md={7}  >
         <h3 className={Styles.header}>Recent Transations</h3>
          {expenceList.map((eachExpence)=>  <Transactions data={eachExpence}></Transactions> )}
        
       </Grid>
-      <Grid item xs={12} md={5} lg={5}>
+      <Grid item xs={12} md={5} >
         <TopExpences></TopExpences>
       </Grid>
     
